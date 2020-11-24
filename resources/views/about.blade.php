b01ba6d51fee77297669a37ba940ce02da0124e0
Sangwa-C <chelseasabinesangwa@gmail.com>
2020-11-24 18:11:13 +0200

    today's edits

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,5 +2,72 @@
 
 @section('content')
 
+    <style>
+        body {
+            background-color: azure;
+        }
+
+    </style>
+
+    <div class="container" style="margin-top: 10%">
+        <div style="background-color: white">
+            <h2 class="text-center">~ Our Mission ~</h2>
+            <h6 class="text-center">To connect investors to zeleous enterpreneurs</h6>
+
+            <br><br>
+            <p style="margin-left:8%; margin-right:8%">
+                We are an intermediate company that helps entrepreneurs to connect with investors, either helping
+                with mentorship, trainings, and funding.
+
+                According to what an entrepreneur project needs or investor wants, we categorize them and analyze
+                deeply their targets before matching them. Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Molestias rerum velit, aliquid tempore, accusamus
+                hic at fugiat voluptatibus nulla officia expedita qui! Error, libero voluptatum. Corrupti illo officia optio
+                repellat. <br><br>
+            </p>
+
+
+        </div>
+
+        <div style="background-color: white">
+            <h2 class="text-center" style="padding-top:3%">~ Leadership ~</h2>
+            <br><br>
+            <div class="container row">
+
+                <div class="col-md-4 justfiy-content-center">
+                    <img src="https://www.pngitem.com/pimgs/m/30-307416_profile-icon-png-image-free-download-searchpng-employee.png"
+                        alt="" style="width:200px; border-radius:50%">
+                    <h5>FirstName SecondName</h5>
+                    <p class="text-secondary">founder and CEO</p>
+                    <br>
+                    <p class="text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat quaerat
+                        ratione dolorem distinctio
+                        beatae cupiditate optio error corporis similique inventore at adipisci!</p>
+                </div>
+                <div class="col-md-4 justfiy-content-center">
+                    <img src="https://www.pngitem.com/pimgs/m/30-307416_profile-icon-png-image-free-download-searchpng-employee.png"
+                        alt="" style="width:200px; border-radius:50%">
+                    <h5>FirstName SecondName</h5>
+                    <p class="text-secondary">founder and CEO</p>
+                    <br>
+                    <p class="text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat quaerat
+                        ratione dolorem distinctio
+                        beatae cupiditate optio error corporis similique inventore at adipisci!</p>
+                </div>
+                <div class="col-md-4 justfiy-content-center">
+                    <img src="https://www.pngitem.com/pimgs/m/30-307416_profile-icon-png-image-free-download-searchpng-employee.png"
+                        alt="" style="width:200px; border-radius:50%">
+                    <h5>FirstName SecondName</h5>
+                    <p class="text-secondary">founder and CEO</p>
+                    <br>
+                    <p class="text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat quaerat
+                        ratione dolorem distinctio
+                        beatae cupiditate optio error corporis similique inventore at adipisci!</p>
+                </div>
+
+            </div>
+        </div>
+        <br><br>
+    </div>
 
 @endsection
