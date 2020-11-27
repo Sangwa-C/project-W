858d2067e6b9544fe398bd627fa54904bd865d31
bazimya <bazimyas@gmail.com>
2020-11-27 19:14:56 +0200

    about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -36,38 +36,45 @@
         <div style="background-color: white">
             <h2 class="text-center" style="padding-top:3%">~ Leadership ~</h2>
             <br><br>
+            <h2 class="text-center" style="padding-top:3%"> <u>Join venture</u>  </h2>
+            <br><br>
             <div class="container row">
 
                 <div class="col-md-4 justfiy-content-center">
-                    <img src="https://www.pngitem.com/pimgs/m/30-307416_profile-icon-png-image-free-download-searchpng-employee.png"
+                    <img src="{{ url('assets/img/test3.png') }}"
                         alt="" style="width:200px; border-radius:50%">
-                    <h5>FirstName SecondName</h5>
-                    <p class="text-secondary">founder and CEO</p>
+                    <h5>IT company</h5>
+                    <p class="text-secondary">Co-founder</p>
                     <br>
-                    <p class="text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat quaerat
-                        ratione dolorem distinctio
-                        beatae cupiditate optio error corporis similique inventore at adipisci!</p>
+                    <p class="text-justify">YANJYE is a Platform that was established by enthusiastic IT professionals,
+                         the forum focuses on creating IT solutions to other learners who are passionate to develop
+                         their IT capabilities and have not had a chance to this effect. The platform looks forward to
+                         developing skill sets in programming, Software development, and other IT skill sets.
+                        The platform looks forward to mentor and develops trainees at all levels regardless of their IT background by teaching them from scratch coding language.
+                        <a href="https://yanjye.com/about-us" class="text-danger">more</a>
+                    </p>
+
                 </div>
                 <div class="col-md-4 justfiy-content-center">
-                    <img src="https://www.pngitem.com/pimgs/m/30-307416_profile-icon-png-image-free-download-searchpng-employee.png"
-                        alt="" style="width:200px; border-radius:50%">
-                    <h5>FirstName SecondName</h5>
-                    <p class="text-secondary">founder and CEO</p>
-                    <br>
-                    <p class="text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat quaerat
-                        ratione dolorem distinctio
-                        beatae cupiditate optio error corporis similique inventore at adipisci!</p>
+                    <div class="text-center">
+                        <img src="{{ url('assets/img/connecting.png') }}" class="rounded" alt="..."  style="width:200px; border-radius:50%">
+                      </div>
+
                 </div>
-                <div class="col-md-4 justfiy-content-center">
-                    <img src="https://www.pngitem.com/pimgs/m/30-307416_profile-icon-png-image-free-download-searchpng-employee.png"
-                        alt="" style="width:200px; border-radius:50%">
-                    <h5>FirstName SecondName</h5>
-                    <p class="text-secondary">founder and CEO</p>
+
+                <div class="col-md-4 justfiy-content-center " >
+
+                    <img src="{{ url('assets/img/envisage.png') }}"  alt="" style="width:200px; border-radius:50%">
+                    <h5>INCUBATORS Company</h5>
+                    <p class="text-secondary">co-founder</p>
                     <br>
-                    <p class="text-justify">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat quaerat
-                        ratione dolorem distinctio
-                        beatae cupiditate optio error corporis similique inventore at adipisci!</p>
+                    <p class="text-justify">Envisage Kigali was inaugurated by the Minister of Agriculture in August 2019,
+                        it’s the first incubator launched by Envisage. At the Kigali location we offer promising early-stage agribusiness startups a 6-months long
+                         incubation program. The space is located at the BDF House in Gatenga. The office is creative and functional, with 32 hot desks, a lounge and a meeting room.
+                         <a href="https://www.envisageincubators.com/goals" class="text-danger">more</a>
+                        </p>
                 </div>
+
 
             </div>
         </div>
